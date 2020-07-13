27deb3ebd3d4c0a05a06a7da69a51fe0e7d2920a
Zin Bhone Htut <zinbhonehtut@gmail.com>
2020-07-14 00:05:58 +0630

    initial project add

---
+++ src/test/java/com/zbh/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbh.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
