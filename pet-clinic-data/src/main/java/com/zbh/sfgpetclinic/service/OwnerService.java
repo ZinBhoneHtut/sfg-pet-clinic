f3586d0601f83074221061211cf368e2b65ffb6c
Zin Bhone Htut <ZinBhoneHtut@startinpoint.com>
2021-09-11 21:32:05 +0630

    Refactor (service) : refactor services to base common service, close #10

--- pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/service/OwnerService.java
@@ -1,16 +1,8 @@
 package com.zbh.sfgpetclinic.service;
 
-import java.util.Set;
-
 import com.zbh.sfgpetclinic.model.Owner;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
 	Owner findByLastName(String lastName);
-	
-	Owner findById(Long id);
-	
-	Owner save(Owner owner);
-	
-	Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/service/VetService.java
@@ -1,14 +1,6 @@
 package com.zbh.sfgpetclinic.service;
 
-import java.util.Set;
-
 import com.zbh.sfgpetclinic.model.Vet;
 
-public interface VetService {
-
-	Vet findById(Long id);
-	
-	Vet save(Vet vet);
-	
-	Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
