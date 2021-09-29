4728ad5cfed51bb32c140b818a7bf3b9fbdcc767
Zin Bhone Htut <ZinBhoneHtut@startinpoint.com>
2021-09-29 22:48:24 +0630

    Feature (entity) : add visit entity and update owner with pet relationship, closes 26

--- pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/model/Owner.java
@@ -1,7 +1,20 @@
 package com.zbh.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
 	
 	private static final long serialVersionUID = -8600280741618339062L;
+	
+	private Set<Pet> pets;
 
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
+	
 }
