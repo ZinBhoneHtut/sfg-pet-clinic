3b45a422b7fd7104529fee899cdda45d193fa32a
SIPDOMAIN\zinbhonehtut <zinbhonehtut@gmail.com>
2021-10-24 22:35:22 +0630

    Refactor (entity) : convert owner to jpa entity, closes #36

--- pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/model/Owner.java
@@ -2,13 +2,28 @@ package com.zbh.sfgpetclinic.model;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
 	
 	private static final long serialVersionUID = -8600280741618339062L;
 	
+	@Column(name = "address")
 	private String address;
+	
+	@Column(name = "city")
 	private String city;
+	
+	@Column(name = "telephone")
 	private String telephone;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
 	private Set<Pet> pets;
 	
 	public String getAddress() {
