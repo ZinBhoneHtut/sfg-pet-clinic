ab086b492b55b8a24e54933b14168258205fb625
SIPDOMAIN\zinbhonehtut <zinbhonehtut@gmail.com>
2021-10-24 22:51:46 +0630

    Refactor (entity) : convert vet to jpa entity, closes #37

--- pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/model/Speciality.java
@@ -1,13 +1,20 @@
 package com.zbh.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
 * @author ZinBhoneHtut
 * @date Sep 29, 2021
 */
-public class Speciality extends Person {
+@Entity
+@Table(name = "specialities")
+public class Speciality extends BaseEntity {
 
 	private static final long serialVersionUID = 8219591189702919507L;
 	
+	@Column(name = "description")
 	private String description;
 
 	public String getDescription() {
--- pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/zbh/sfgpetclinic/model/Vet.java
@@ -2,10 +2,21 @@ package com.zbh.sfgpetclinic.model;
 
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 	
 	private static final long serialVersionUID = 6668050596873100596L;
 
+	@ManyToMany
+	@JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+		inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<Speciality> specialities;
 
 	public Set<Speciality> getSpecialities() {
