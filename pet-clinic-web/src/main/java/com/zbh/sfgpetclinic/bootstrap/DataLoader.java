bf995e06699366e062b0ad0d9a71ad182620ec01
Zin Bhone Htut <ZinBhoneHtut@startinpoint.com>
2021-10-28 08:24:23 +0630

    enhance (startup) : enhance owner with contact info and pets on startup, closes #33

--- pet-clinic-web/src/main/java/com/zbh/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/zbh/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,9 +1,12 @@
 package com.zbh.sfgpetclinic.bootstrap;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.zbh.sfgpetclinic.model.Owner;
+import com.zbh.sfgpetclinic.model.Pet;
 import com.zbh.sfgpetclinic.model.PetType;
 import com.zbh.sfgpetclinic.model.Speciality;
 import com.zbh.sfgpetclinic.model.Vet;
@@ -34,19 +37,42 @@ public class DataLoader implements CommandLineRunner {
 		PetType dog = new PetType();
 		dog.setName("Dog");
 		petTypeService.save(dog);
+		PetType savedDogPetType = petTypeService.save(dog);
 
 		PetType cat = new PetType();
 		cat.setName("Cat");
-		petTypeService.save(cat);
+		PetType savedCatPetType = petTypeService.save(cat);
 
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Zephania");
 		owner1.setLastName("Ingram");
-		ownerService.save(owner1);
+		owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1231231234");
+        
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
+        
+		ownerService.save(owner1);	
 
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Charde");
 		owner2.setLastName("Fitzgerald");
+		owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("1231231234");
+        
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
+        
 		ownerService.save(owner2);
 
 		Speciality radiology = new Speciality();
